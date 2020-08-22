a37340870604713277f14b6e209e8b9d35e52b94
chriskimgts <65778487+chriskimgts@users.noreply.github.com>
2020-08-22 18:44:09 -0400

    Finished display a recipe by ID

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,59 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package guru.springframework.repositories;
 
 import guru.springframework.domain.UnitOfMeasure;
-import java.util.Optional;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
 /**
- *
- * @author chris
+ * Created by jt on 6/17/17.
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UnitOfMeasureRepositoryIT {
-    
+
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
-    
-    public UnitOfMeasureRepositoryIT() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
+
     @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+    public void setUp() throws Exception {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-     @Test
-     public void findByDescription() throws Exception{
-         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-         assertEquals("Teaspoon", uomOptional.get().getDescription());
-     }
-     @Test
-     public void findByDescriptionCup() throws Exception{
-         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
-         assertEquals("Cup", uomOptional.get().getDescription());
-     }
+    @Test
+    public void findByDescription() throws Exception {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+
 }
