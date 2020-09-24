6884f57324d185ec08dd573059bb595932c7374f
chris <chris@DESKTOP-JD8Q0KC>
2020-09-24 09:58:23 -0400

    mysql password encripted as adding jasypt

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -1,9 +1,11 @@
 package guru.springframework;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
